5de48881e1d54b237cfc54b2b14e06b6e64ae95d
Stephanie Marzan <s.marzanoberholz@ufl.edu>
2022-04-06 19:43:02 -0500

    created car class with properties and two methods

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // YOUR CODE HERE
+
+//your assignment is to create a class named `Car` in JavaScript. This class will create objects that have three properties:
+
+//1. `make` (String) - The car's manufacturer. _E.g. Honda_
+//1. `model` (String) - The type of model. _E.g. Civic_
+//1. `year` (Integer) - Four digit year the car was made. _E.g. 1998
+
+//setTimeout(function() {
+    //(console.log(maintenanceMessage), 3000);
+//Your class should also have the following methods:
+
+//- `honk` - This method should log "BEEP BEEP!" to the console when called.
+//- `performMaintenance` - This method should use the `setTimeout` function to log "maintenance complete" to the console **after exactly 3 seconds**.
+
+class Car {
+    constructor( make, model, year){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    honk (){
+        console.log('BEEP BEEP!')
+    }
+    performMaintenance(){
+        let maintenanceMessage = "Maintenance Complete";
+        setTimeout (function(){
+            console.log(maintenanceMessage), 3000})
+    }
+
+}
